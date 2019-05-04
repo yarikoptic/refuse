95cba4627010f94bdd700023beca0f8bbafd7dcd
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-05-04 23:54:19 +0200

    cleaned up setup

--- setup.py
+++ setup.py
@@ -1,36 +1,37 @@
 #!/usr/bin/env python
 
-from __future__ import with_statement
-
-from setuptools import setup
+from setuptools import find_packages, setup
 
 _version_ = '0.0.1' # BUMP VERSION HERE!
 
-with open('README') as readme:
-    documentation = readme.read()
+with open('README.md') as readme:
+    long_description = readme.read()
 
 setup(
-    name = 'fusepy',
+    name = 'refuse',
+	packages = find_packages('src'),
+	package_dir = {'': 'src'},
     version = _version_,
-
-    description = 'Simple ctypes bindings for FUSE',
-    long_description = documentation,
-    author = 'Giorgos Verigakis',
-    author_email = 'verigak@gmail.com',
-    maintainer = 'Terence Honles',
-    maintainer_email = 'terence@honles.com',
+    description = 'Simple ctypes bindings for libfuse / FUSE',
+    long_description = long_description,
+    author = 'Sebastian M. Ernst',
+    author_email = 'ernst@pleiszenburg.de',
+    url = 'https://github.com/pleiszenburg/refuse',
     license = 'ISC',
-    py_modules=['fuse'],
-    url = 'http://github.com/fusepy/fusepy',
-
+    keywords = ['fuse', 'libfuse'],
+    include_package_data = True,
+    install_requires = [],
+    zip_safe = False,
     classifiers = [
+		'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Operating System :: MacOS',
         'Operating System :: POSIX',
+		'Operating System :: POSIX :: BSD',
+		'Operating System :: POSIX :: Linux',
         'Operating System :: Unix',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 3',
         'Topic :: System :: Filesystems',
-    ]
-)
+        ]
+    )
