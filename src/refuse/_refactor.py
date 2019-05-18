da1d6c199d675babd59471625d2f28841c9ac97f
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-05-18 18:15:38 +0200

    new helper sub module for refactoring

---
+++ src/refuse/_refactor.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+REFUSE
+Simple cross-plattform ctypes bindings for libfuse / FUSE for macOS / WinFsp
+https://github.com/pleiszenburg/refuse
+
+    src/refuse/_refactor.py: Temporary helper for code refactoring
+
+    THIS FILE IS TEMPORARY AND WILL BE REMOVED!
+
+    Copyright (C) 2008-2019 refuse contributors
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the Internet Systems Consortium (ISC)
+license ("ISC license" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://opensource.org/licenses/ISC
+https://github.com/pleiszenburg/refuse/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
