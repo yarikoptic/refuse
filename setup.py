63a2da5304c57cb799168f8e4561fbac4dce2496
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-07-10 20:41:26 +0200

    added (and tested) python 3.8 support

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ _version_ = '0.0.4'
 # List all versions of Python which are supported
 confirmed_python_versions = [
     ('Programming Language :: Python :: %s' % x)
-    for x in '3.5 3.6 3.7'.split(' ')
+    for x in '3.5 3.6 3.7 3.8'.split(' ')
     ]
 
 # Fetch readme file
